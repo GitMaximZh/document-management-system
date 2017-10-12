89f1bff4ab78985d4486d0c7fdfb4a774da2be33
Maksim Zhukov <m.zhukov@amm.net>
2017-10-12 18:41:15 +0200

    docker

---
+++ prototype/DMS.WebAPI/Deploy/build-image.bat
@@ -0,0 +1,9 @@
+﻿dotnet restore ../
+dotnet publish ../  -c release -o ./obj/Docker/publish
+
+docker build ../ -t dms-web-api
+
+docker-compose run
+
+#docker-compose -f compose-dms-mongodb.yml up
+#ipconfig to get ip
